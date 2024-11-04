b22dcda806b6a82b554f5b726de5ce802408d279
fajri1997 <f.a.f.s.m.alhusaini@edu.salford.ac.uk>
2024-11-04 08:25:38 +0000

    Methods-java fixed and add step 6

--- src/SummationPractice.java
+++ src/SummationPractice.java
@@ -6,11 +6,13 @@ public class SummationPractice {
     public static void main(String[] args) {
 
 
-//        int[] arrayList  = {1,2,3,5,6,7,12,34,66,78};
-//        SummationPractice(ArrayList)
+
         List<Integer> arrayList = new ArrayList<>(List.of(1, 2, 3, 5, 6, 7, 12, 34, 66, 78));
 
-        System.out.println(summationPractice(arrayList));
+
+        int totalSum = summationPractice(arrayList);
+        System.out.println("The sum of the list is: " + totalSum);
+
     }
 
     static int summationPractice(List<Integer> nums) {
