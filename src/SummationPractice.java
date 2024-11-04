41975ecffba6be4bba4044d550310aad60721893
fajri1997 <f.a.f.s.m.alhusaini@edu.salford.ac.uk>
2024-11-04 08:18:09 +0000

    Methods-java fixed

--- src/SummationPractice.java
+++ src/SummationPractice.java
@@ -6,14 +6,14 @@ public class SummationPractice {
     public static void main(String[] args) {
 
 
-//        int[] ArrayList  = {1,2,3,5,6,7,12,34,66,78};
+//        int[] arrayList  = {1,2,3,5,6,7,12,34,66,78};
 //        SummationPractice(ArrayList)
-        List<Integer> ArrayList = new ArrayList<>(List.of(1, 2, 3, 5, 6, 7, 12, 34, 66, 78));
+        List<Integer> arrayList = new ArrayList<>(List.of(1, 2, 3, 5, 6, 7, 12, 34, 66, 78));
 
-        System.out.println(SummationPractice(ArrayList));
+        System.out.println(summationPractice(arrayList));
     }
 
-    static int SummationPractice(List<Integer> nums) {
+    static int summationPractice(List<Integer> nums) {
         int sum = 0;
         for (int number : nums) {
             sum += number;
